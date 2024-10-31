9e4ba6fcb61e2bf79d1ba7279bb75aae6fc63dbc
Fakhita123* <farhan.mustapha2016@gmail.com>
2024-10-31 21:38:11 +0100

    afficher list

---
+++ p4.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app=FastAPI()
+
+crew = [
+    {"id": 1, "name": "Cosmo", "role": "Captain"},
+    {"id": 2, "name": "Alice", "role": "Engineer"},
+    {"id": 3, "name": "Bob", "role": "Scientist"}
+]
+@app.get("/crew")
+def home():
+    return {"coo":crew}
+
+if __name__=="__main__":
+    uvicorn.run(app,host="127.0.0.1",port=8001)
